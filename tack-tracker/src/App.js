8ed1db8956ca947829705fcee1c83d6518024256
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2022-11-01 14:59:28 +0300

    Add Tasks component and use useSate.

--- tack-tracker/src/App.js
+++ tack-tracker/src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Tarvosa Report Customization',
+      day: 'Nov 3rd at 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Login Page for Dashborad',
+        day: 'Nov 5th at 12:00',
+        reminder: true,
+    }
+  ])
+
   return (
     <div className="container">
       <Header title={'React Task Tracker'}/>
+      <Tasks tasks={tasks} />
       <Footer />
     </div>
   );
