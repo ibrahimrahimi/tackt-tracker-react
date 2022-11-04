6b982dfdda829e3ceca5c6a0b345d177addd0dde
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2022-11-04 21:17:03 +0300

    Add create and update tasks to server featuer.

--- tack-tracker/src/App.js
+++ tack-tracker/src/App.js
@@ -26,12 +26,31 @@ function App() {
     return data
   }
 
-  // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000 + 1)
+  // Fetch Task
+  const fetchTask = async(id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
 
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+    return data
+  }
+
+  // Add Task
+  const addTask = async(task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = res.json()
+    setTasks([...tasks, task])
+
+    // const id = Math.floor(Math.random() * 10000 + 1)
+
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
@@ -41,10 +60,23 @@ function App() {
   }
 
   // Toggle Tasks
-  const toggleReminder = (id) => {
+  const toggleReminder = async(id) => {
+    const taskToToggle = await fetchTask(id)
+    const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updateTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) => 
-      task.id === id ? {...task, reminder: !task.reminder} : task)
+      task.id === id ? {...task, reminder: data.reminder} : task)
     )
   }
 
